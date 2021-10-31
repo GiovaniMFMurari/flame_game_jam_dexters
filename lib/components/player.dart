f8774aa8c3779373184e291f7adcbaa13d5a3ae7
Icaro-G-Silva <icarogabrielsilva2019@gmail.com>
2021-10-31 12:24:06 -0300

    Add start screen

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -4,11 +4,9 @@ import 'package:flame_game_jam_dexters/main.dart';
 class Player extends SpriteAnimationComponent with HasGameRef<MyGame> {
   static final dimensions = Vector2(32, 32);
 
-  @override
-  void onGameResize(Vector2 gameSize) {
-    y = gameSize.y - height / 2;
-    x = gameSize.x / 2;
-    super.onGameResize(gameSize);
+  Player() {
+    height = 128;
+    width = 128;
   }
 
   @override
@@ -17,11 +15,6 @@ class Player extends SpriteAnimationComponent with HasGameRef<MyGame> {
 
     anchor = Anchor.center;
 
-    width = 128;
-    height = 128;
-
-    y = gameRef.size.y - height / 2;
-    x = gameRef.size.x / 2;
     animation = await gameRef.loadSpriteAnimation(
       'ghost.png',
       SpriteAnimationData.sequenced(
