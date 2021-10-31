2ae9693ddbe22561b171e02c39c94e5ba833ae49
Edylson Torikai <edylson.torikai@dextra-sw.com>
2021-10-31 01:33:17 -0300

    Implement stage manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:collection';
+
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flame_game_jam_dexters/components/background.dart';
-import 'package:flame_game_jam_dexters/components/match.dart';
-import 'package:flame_game_jam_dexters/components/trick.dart';
+import 'package:flame_game_jam_dexters/components/item.dart';
+import 'package:flame_game_jam_dexters/components/stage.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
@@ -15,12 +17,39 @@ void main() {
 
 class MyGame extends FlameGame with DoubleTapDetector, TapDetector {
   final Background _background = Background();
+  late Queue stageRows;
 
   @override
   Future<void>? onLoad() async {
     await super.onLoad();
     add(_background);
-    add(Trick.empty());
-    add(Match.empty());
+
+    stageRows = Stage(stage: 1).readStage();
+  }
+
+  double time = 0.0;
+
+  bool shouldRender = false;
+
+  @override
+  void update(double dt) {
+    time += dt;
+
+    if (time % 0.2 > 0.1) {
+      shouldRender = true;
+    }
+
+    if (stageRows.isNotEmpty && shouldRender && time % 0.2 < 0.1) {
+      List<Item?> currentRow = stageRows.removeFirst();
+      for (var item in currentRow) {
+        if (item != null) {
+          add(item);
+        }
+      }
+
+      shouldRender = false;
+    }
+
+    super.update(dt);
   }
 }
